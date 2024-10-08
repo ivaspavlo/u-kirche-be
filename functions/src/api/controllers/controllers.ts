be100dfd3054548cd7697e21cae6d6254de86969
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2024-10-08 06:58:47 +0200

    UK-3: Add debug config

--- functions/src/api/controllers/controllers.ts
+++ functions/src/api/controllers/controllers.ts
@@ -3,10 +3,12 @@ import { RootController } from './root-controller';
 import { AccountController } from './account-controller/account-controller';
 import { ProductController } from './product-controller/product-controller';
 import { AdminController } from './administrative-controller/admin-controller';
+import { ArticleController } from './articles-controller/articles-controller';
 
 export const controllers: Array<Controller> = [
     new RootController(),
     new ProductController(),
     new AccountController(),
-    new AdminController()
+    new AdminController(),
+    new ArticleController()
 ];

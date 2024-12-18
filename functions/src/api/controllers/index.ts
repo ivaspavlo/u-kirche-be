3ea280f6ec845281593fe51eb3bae1112ac99719
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2024-12-18 08:35:03 +0100

    UK-16: Start working on meet functionality

--- functions/src/api/controllers/index.ts
+++ functions/src/api/controllers/index.ts
@@ -4,11 +4,13 @@ import { ArticleController } from './articles-controller';
 import { UserController } from './user-controller';
 import { AuthController } from './auth-controller';
 import { ContentController } from './content-controller';
+import { MessageController } from './message-controller';
 
 export const controllers: Controller[] = [
     new RootController(),
     new ArticleController(),
     new UserController(),
     new AuthController(),
-    new ContentController()
+    new ContentController(),
+    new MessageController()
 ];
---
+++ functions/src/api/controllers/message-controller.ts
@@ -0,0 +1,16 @@
+import { RequestHandler } from 'express';
+import { Controller } from '../../core/interfaces';
+import { HttpServer } from '../../core/utils/http-server';
+
+export class MessageController implements Controller {
+    initialize(httpServer: HttpServer): void {
+        httpServer.post('/meet', this.meet.bind(this), []);
+    }
+
+    private readonly meet: RequestHandler = async (_, res, next) => {
+        res.send({
+            status: `API is working!`
+        });
+        next();
+    };
+}

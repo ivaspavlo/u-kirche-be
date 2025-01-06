bfe9b14b0cb7655cbb426f9d4090f9c6f4ec126e
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2025-01-06 09:06:47 +0100

    UK-23: Integrate rate limit package

--- functions/src/api/middleware/index.ts
+++ functions/src/api/middleware/index.ts
@@ -3,17 +3,23 @@ import bodyParser from 'body-parser';
 import { Request, Response, NextFunction } from 'express';
 import { verifyJwtMiddleware } from './verify-jwt.middleware';
 import { ENV_KEY } from '../../core/constants';
-
-console.log(process.env[ENV_KEY.UI_ORIGIN]);
+import rateLimit from 'express-rate-limit';
 
 export const middleware: Array<(req: Request, res: Response, next: NextFunction) => void> = [
+    rateLimit({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 100, // Limit each IP to 100 requests per 15 minutes
+        message: 'Too many requests from this IP address, please try again later',
+        standardHeaders: true, // Return rate limit info in the 'RateLimit-*' header
+        legacyHeaders: false // Disable the 'X-RateLimit-*' headers
+    }),
     bodyParser.urlencoded({ extended: false }),
     bodyParser.json(),
     cors({
         origin: process.env[ENV_KEY.UI_ORIGIN],
         optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
     }),
-    // Setting default values
+    // Setting default claim values
     (req, _, next) => {
         req.claims = {} as any;
         next();
